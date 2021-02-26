e986eda6b99bd913409d68724b13e41c4c7501ed
yinm <yinm.cminor9@gmail.com>
2021-02-26 11:45:33 +0900

    fix typo

--- functions/hello.js
+++ functions/hello.js
@@ -1,5 +1,5 @@
 exports.handler = async (event) => {
-  const subject = event.queryStringParameter.name || "World";
+  const subject = event.queryStringParameters.name || "World";
   return {
     statusCode: 200,
     body: `Hello ${subject}`,
